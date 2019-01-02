725391271ab927ff271c95bb6c4fba2ced9c4661
puriwat3804 <46322210+puriwat3804@users.noreply.github.com>
2019-01-03 00:47:02 +0700

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int A,B,X;
+	printf("First Number is ");
+	scanf("%d",&A);
+	printf("Second Number is ");
+	scanf("%d",&B);
+	X = A * B;
+	printf("The Result is ");
+	printf("%d",X);
+	getch();
+}
